c403bec38a2a374e7b0fc21ee2751770ebb3db8c
Baxtter21 <104280965+Baxtter21@users.noreply.github.com>
2022-08-17 17:49:16 -0300

    Arquivos do Projeto

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+Referência: 
+Bonatti Denilson, https://github.com/denilsonbonatti/toshiro-shibakita, acessado em 17 de Agosto de 2022, às 17h41min.
---
+++ banco.sql
@@ -0,0 +1,10 @@
+echo "criando tabelas de dados dos clientes..."
+
+CREATE TABLE dados (
+    AlunoID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
